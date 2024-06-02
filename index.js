8884da2629818956757d87f614e061aad5b01f70
Shruti211431 <shruti.211431.it@mhssce.ac.in>
2024-06-02 18:52:01 +0530

    index.js modified

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from 'express'
 import colors from 'colors'
+import { errorHandler, notFound } from './middlewares/error.middleware';
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is listening at ${PORT}`.blue.underline)
